70f0beb57404695d74d93ec965c1a1707f182fb2
Soumyadeep Thakur <soumyadeep.thakur@gmail.com>
2017-04-06 01:31:06 +0530

    Update ODD_EVEN.asm

--- SEM-4/Assembly-8085/ODD_EVEN.asm
+++ SEM-4/Assembly-8085/ODD_EVEN.asm
@@ -1,23 +1,25 @@
-LDA 204FH
-STA 204EH
-LXI H,2500H
-LXI B,2100H
-LXI D,2200H
+;seperate out odd and even no from a given sequence of numbers
+
+START: LDA 204FH ;length of sequence
+STA 204EH ;store no of elements in temprary location
+LXI H,2500H ;numbers in sequence
+LXI B,2100H ;even numbers
+LXI D,2200H ;odd numbers
 LOOP: MOV A,M
 RAR
-JC ODD
-RAL
-STAX B
+JC ODD ;check for odd
+RAL ;restore number
+STAX B ;store even number in [BC]
 LDA 2300H
 INR A
-STA 2300H
+STA 2300H ;increment count of even numbers
 INX B
 JMP NEXT
-ODD: RAL
-STAX D
+ODD: RAL ;restore the number
+STAX D ;store odd number in [DE] location
 LDA 2301H
 INR A
-STA 2301H
+STA 2301H ;increment count of odd number
 INX D
 NEXT: INX H
 LDA 204EH

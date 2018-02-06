7be0fa8183f164735ffba7b0afca55678d7c52e5
Soumyadeep Thakur <soumyadeep.thakur@gmail.com>
2018-02-06 17:25:09 +0530

    Update code

--- SEM-6/lex_analyser.cpp
+++ SEM-6/lex_analyser.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include <cstdlib>
 #include <cstdio>
 #include <sstream>
@@ -9,18 +9,21 @@
 using namespace std;
 
 class token_entry;
-//enum token_type = {arithop, relop, assignop, identifier, number};
+enum token_type {KEYWORD, IDENTIFIER, LITERAL, ARITHOP, RELOP, ASSIGNOP, PUNCTUATION};
 std::unordered_set<std::string> keyword;
 std::vector<token_entry> token_table;
 
 class token_entry
 {
 	public:
-	std::string tokentype, lexeme;
-	int row, col;
-	token_entry(std::string tok_type, std::string lex, int row_no, int col_no):tokentype{tok_type}, lexeme{lex}, row{row_no}, col{col_no} {};
-
+	std::string lexeme;
+	int tokentype, row, col;
+	token_entry(int tok_type, std::string lex, int row_no, int col_no):tokentype{tok_type}, lexeme{lex}, row{row_no}, col{col_no} {};
 };
+void trim_left_copy(std::string& s, const std::string& delimiters = " \f\n\r\t\v" )
+{
+	s.erase(s.find_first_not_of( delimiters));
+}
 bool is_valid_identifier(std::string id)
 {
 	if (!(id[0] == '_' || (id[0] <= 'Z' && id[0] >= 'A') || (id[0] <= 'z' && id[0] >= 'a'))) return false;
@@ -45,23 +48,57 @@ bool is_number(std::string token)
 	}
 	return true;
 }
+bool is_arith_op(std::string token)
+{
+	if (token.length() == 1)
+	{
+		char c = token[0];
+		if (c=='+' || c=='-' || c=='*' || c=='/')
+			return true;
+	}
+	return false;
+}
 void populate_keyword()
 {
 	std::ifstream keyfile;
 	keyfile.open("key.txt", ios::in);
 	std::string keywd;
-	keyfile >> keywd;
-	keyword.insert(keywd);
+	while (!keyfile.eof())
+	{
+		keyfile >> keywd;
+		keyword.insert(keywd);
+	}
+	//cout << "Keywords: " << endl;
+	//for (std::string keyw: keyword) cout << keyw << " ";
+	//cout << endl;
 	keyfile.close();
 }
 int get_token_type(std::string token)
 {
+	int len = token.length();
 	
+	if (is_keyword(token))
+		return KEYWORD;
+	if (is_number(token))
+		return LITERAL;
+	if (is_valid_identifier(token))
+		return IDENTIFIER;
+	if (token.length() == 1)
+	{
+		char c = token[0];
+		if (c=='=') return ASSIGNOP;
+		else if (c=='+' || c=='-' || c=='*' || c=='/') return ARITHOP;
+		else if (c=='>' || c=='<') return RELOP;
+		else if (c==';' || c== ',' || c=='(' || c== ')' || c=='{' || c== '}') return PUNCTUATION;
+	}
+	return -1;
+		
 }
-void tokenize(std::vector<std::string>& tokens, std::string& line)
+void tokenize(std::string& line, std::vector<std::string>& tokens, std::vector<int>& col_no)
 {
 	std::string delimiter = " +-*/<>=;,(){}";
 	int last = 0, len = 0;
+	//trim_left_copy(line); // handle left tabs here pls
 	line = line + " ";
 	for (int i = 0; i < line.length(); i++)
 	{
@@ -74,11 +111,13 @@ void tokenize(std::vector<std::string>& tokens, std::string& line)
 			{
 				//cout << "Token: " << line.substr(last, len) << endl;
 				tokens.push_back(line.substr(last, len));
+				col_no.push_back(last);
 			}
 			if (c!=' ') 
 			{
 				//cout << "delim pushed" << endl;
 				tokens.push_back(std::string(1,c));
+				col_no.push_back(i);
 			}
 			last = i+1;
 			len=0;
@@ -97,25 +136,33 @@ void process_code(std::string code_file_name)
 	std::fstream code_file(code_file_name, ios::in);
 	std::string code_line;
 	std::vector<string> tokens;
+	std::vector<int> cols;
+	int line_no = 1;
 	cout << "Hi" << endl;
 	//code_file >> code_line;
 	//cout << code_line;
+	populate_keyword();
 	while (!code_file.eof())
 	{
-		std::string tokentype;
+		int tokentype;
 		getline(code_file, code_line);
 		cout << code_line << endl;
-		tokenize(tokens, code_line);
+		tokenize(code_line, tokens, cols);
 		for (std::string& token : tokens)
 		{
 			tokentype = get_token_type(token);
 			// add token to table
-			//token_entry t_ent(tokentype, token, )
-
+			cout << "Tokentype: " << tokentype;
+			token_entry t_ent(tokentype, token, line_no, cols.front()+1);
+			cols.erase(cols.begin());
+			token_table.push_back(t_ent);
 		}
+
 		std::vector<std::string>().swap(tokens); // free vector memory
+		line_no++;
 	}
 }
+
 int main(int argc, char const **argv)
 {
 	process_code("code.txt");
@@ -129,5 +176,10 @@ int main(int argc, char const **argv)
 	//for (std::string& s : tokens)
 	//	cout << s << ",";
 	//cout << endl;
+	cout << "Token Table: " << endl;
+	for (token_entry& t : token_table)
+	{
+		cout << t.tokentype << " - " << t.lexeme << " - " << t.row << " - " << t.col << endl;
+	}
 	return 0;
 }
